8e78efafdbe9d6911adce53402f6a8750b0fb32f
KorenevaIV <bagira-87@mail.ru>
2022-11-06 16:04:19 +0300

    add second solution

--- 3/3.4.py
+++ 3/3.4.py
@@ -1,11 +1,17 @@
 # Пользователь вводит 3 числа, сказать сколько из них  положительрных и сколько отрицательных
 # первый способ на определение положительное или отрицательное
-first_num = input('Первое число: ')
-print(first_num.isdigit())
-second_num = input('Второе число: ')
-print(second_num.isdigit())
-third_num = input('Третье число: ')
-print(third_num.isdigit())
+# first_num = input('Первое число: ')
+# print(first_num.isdigit())
+# second_num = input('Второе число: ')
+# print(second_num.isdigit())
+# third_num = input('Третье число: ')
+# print(third_num.isdigit())
 
 # Дальше случился ступор, как без использование  if  и счетчика посчитать сколько каких чисел.
 # Наведи пожалуйста на верное решение
+first = input()
+second = input()
+third = input()
+summ = first + second + third
+print('Число отрицательных чисел: ', summ.count('-'))
+print('Число положительных чисел: ', (3 - summ.count('-')))
