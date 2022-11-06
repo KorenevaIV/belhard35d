66c6506dd9045cbb34ca63cf4046a0177357ca9e
KorenevaIV <bagira-87@mail.ru>
2022-11-06 14:45:27 +0300

    add first  solution

--- 3/3.4.py
+++ 3/3.4.py
@@ -1,5 +1,8 @@
 # Пользователь вводит 3 числа, сказать сколько из них  положительрных и сколько отрицательных
 
-first_num = int(input('Первое число: '))
-second_num = int(input('Второе число: '))
-third_num = int(input('Третье число: '))
+first_num = input('Первое число: ')
+print(first_num.isdigit())
+second_num = input('Второе число: ')
+print(second_num.isdigit())
+third_num = input('Третье число: ')
+print(third_num.isdigit())
