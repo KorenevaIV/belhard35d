e411b71d1565afce0daa61dc17fcc671cf4e5231
KorenevaIV <bagira-87@mail.ru>
2022-11-08 11:11:06 +0300

    add solution

--- 4/4.2.py
+++ 4/4.2.py
@@ -3,3 +3,5 @@ data = {text[i]: text.count(text[i]) for i in range(0, len(text))}
 data1 = {i:text.count(i) for i in text}
 print(data)
 print(data1)
+
+
--- main.py
+++ main.py
@@ -4,7 +4,18 @@
 # print("hello python")
 # print("hello dear")
 
-x = int(input())
-y = int(input())
-print(f'Точка({x = } , {y = })')
+# x = int(input())
+# y = int(input())
+# print(f'Точка({x = } , {y = })')
+
+num = 5
+if num % 2 == 1:
+    print('odd')
+
+word = 'hello2'
+if word.isalpha():
+    print('word')
+else:
+    print('not word')
+
 
