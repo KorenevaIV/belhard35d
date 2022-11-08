55a87a4da1a91ce466f97642532c953ca084ab8c
KorenevaIV <bagira-87@mail.ru>
2022-11-08 10:55:45 +0300

    add solution

--- 4/4.1.py
+++ 4/4.1.py
@@ -1,2 +1,3 @@
-list1 = [2 ** i for i in range(10)]
+n = int(input('n: '))
+list1 = [2 ** i for i in range(n+1)]
 print(list1)
--- 4/4.2.py
+++ 4/4.2.py
@@ -1,3 +1,5 @@
 text = input()
 data = {text[i]: text.count(text[i]) for i in range(0, len(text))}
-data = {i:text.count(i) for i in text}
+data1 = {i:text.count(i) for i in text}
+print(data)
+print(data1)
