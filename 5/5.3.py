f654dc87a990b8bedb456d45239bce2e74c15485
KorenevaIV <bagira-87@mail.ru>
2022-11-10 20:12:42 +0300

    add solution

--- 5/5.3.py
+++ 5/5.3.py
@@ -1,9 +1,14 @@
 # вывести четные числа от 2 до  n по 5 в строку
+# n = int(input())
+# a = [i for i in range(1, n+1) if i % 2 ==0]
+# print(a)
+# while len(a) > 0:
+#     print(a[0:5])
+#     del a[0:5]
+
+
 n = int(input())
-a = [i for i in range(1, n+1)]
-print(a)
+a = [i for i in range(2, n+1,2)]
 while len(a) > 0:
     print(a[0:5])
     del a[0:5]
-
-
