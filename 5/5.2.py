e7aaa56861cf15e196bc6dadc72a3a152c4810a6
KorenevaIV <bagira-87@mail.ru>
2022-11-10 18:15:37 +0300

    add solution

--- 5/5.2.py
+++ 5/5.2.py
@@ -1,9 +1,13 @@
 # сделать калькулятор: пользователь вводит число затеи действие и второе число
-first_num = int(input('Enter number: '))
+first_num = int(input('Enter first number: '))
 text = input('what to do? ')
-second_num = int(input('Enter number: '))
+second_num = int(input('Enter second number: '))
+
 if text == '*':
     print(first_num*second_num)
+elif text == '/':
+    if second_num == 0:
+        print('Please enter other number, not zero')
 elif text == '/':
     print(first_num/second_num)
 elif text == '+':
@@ -13,6 +17,8 @@ elif text == '-':
 elif text == '**':
     print(first_num**second_num)
 else:
-    print('crazy choice')
+    print(f'crazy choice, please try again')
+
+
 
 
