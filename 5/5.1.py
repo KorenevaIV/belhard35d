d240952285e5a56eaa5dd77169a8a9a4c9605e49
KorenevaIV <bagira-87@mail.ru>
2022-11-11 10:51:48 +0300

    add solution

--- 5/5.1.py
+++ 5/5.1.py
@@ -2,13 +2,26 @@
 n = int(input())  #  сколько нужно вывести цифр
 m = int(input())  # кратно этому числу
 k = int(input())  # и больше этого числа
-
-lst = []
-c = 0
-i = k
-while c < n:
-    if (i % m == 0):
-        lst.append(i)
-        c += 1
-    i += 1
-print(*lst)
+#
+# lst = []
+# c = 0
+# i = k
+# while c < n:
+#     if (i % m == 0):
+#         lst.append(i)
+#         c += 1
+#     i += 1
+# print(*lst)
+#
+# while n:
+#     if k%m == 0:
+#         print(k)
+#         n-=1
+#     k += 1
+while n:
+    if k % m == 0:
+        print(k)
+        n -= 1
+        k += m
+    else:
+        k += 1
