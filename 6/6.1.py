2303e6e1797c9f067e91d9ac4730e183111a9d0a
KorenevaIV <bagira-87@mail.ru>
2022-11-13 20:33:24 +0300

    add some data

--- 6/6.1.py
+++ 6/6.1.py
@@ -1,24 +1,36 @@
 #Написать функцию перевода десятичного числа в двоичное и обратно, без
-# использования функции int
+# использования функции int.  def func(n):# ввод числа  двоичного
+#                             print(int(n, base=2)) # приводимм к числу, с основанием(в 2-ичной системе и выоводим)
 def binnum(n):
     s = ''
     while n > 0:
         s = str(n % 2) + s
         n //= 2
     return s
-while 1:
-    n = int(input())
-    if n != 0:
-        print(binnum(n))
-    else:
-        break
 
-s = binnum(n)
-def bin_to_dec(s):
-    dlina = len(s)
-    print(dlina)
+n = int(input())
+if n != 0:
+    print(binnum(n))
+binlist = int(binnum(n))
+print(binlist)
+
+def bin_to_dec(n):
+    dlina = len(n)
     chislo_dec = 0
     for i in range(0, dlina):
-        chislo_dec = chislo_dec + int(s[i]) * (2 ** (dlina - i - 1))
+        chislo_dec = chislo_dec + int(n[i]) * (2 ** (dlina - i - 1))
     return chislo_dec
-print(bin_to_dec(s))
+print(bin_to_dec(binlist))
+
+
+
+
+
+
+#
+# def bin_dec(data):
+#     number = 0
+#     len_dat = len(data)
+#     for i in range(0, len_dat):
+#         number += int(data[i]) * (2**(len_dat - i -1))
+#     return number
