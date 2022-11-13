21865f651fafce52f112303b104d6b49e0afcd70
KorenevaIV <bagira-87@mail.ru>
2022-11-13 20:04:25 +0300

    add some data

---
+++ 6/6.1.py
@@ -0,0 +1,25 @@
+#Написать функцию перевода десятичного числа в двоичное и обратно, без
+# использования функции int
+def binnum(n):
+    global s
+    s = ''
+    while n > 0:
+        s = str(n % 2) + s
+        n //= 2
+    return s
+while 1:
+    n = int(input())
+    if n != 0:
+        print(binnum(n))
+    else:
+        break
+
+
+def bin_to_dec(s):
+    dlina = len(s)
+    print(dlina)
+    chislo_dec = 0
+    for i in range(0, dlina):
+        chislo_dec = chislo_dec + int(s[i]) * (2 ** (dlina - i - 1))
+    return chislo_dec
+
