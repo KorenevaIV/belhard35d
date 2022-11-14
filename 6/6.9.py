38f2018b1f0c5c2e9262be5d5ccf561de187a7a0
KorenevaIV <bagira-87@mail.ru>
2022-11-14 20:46:05 +0300

    add solution

--- 6/6.9.py
+++ 6/6.9.py
@@ -2,3 +2,28 @@
 # словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
 # имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
 # пустая строка)
+
+persons = {1 : {'name':'Ivan',
+                'surname': 'Ivanov',
+                'tel': 123456,
+                'email': 'iidogcat@mail.ru'
+                },
+           2 : {'name': 'Petr',
+                'surname': 'Petrov',
+                'tel': 124587,
+                'email': '' },
+           3 : {'name': 'Toma',
+                'surname': 'Cvetik',
+                'tel': 458966,
+                'email': 'iicvet@mail.ru'}
+
+}
+
+a = persons.keys()
+
+for i in a:
+    print(persons[i].get('email'))
+
+
+
+
