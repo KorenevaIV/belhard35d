ade895322d6a67c134a1b56e591d24e9249d8730
KorenevaIV <bagira-87@mail.ru>
2022-11-14 16:59:21 +0300

    add solution

--- 6/6.5.py
+++ 6/6.5.py
@@ -2,7 +2,14 @@
 # функции reversed, а так же дополнительного списка и среза
 import dis
 
-list1 = [1, 2, 3, 4, 5]
-for i in range(0, len(list1)):
-    num = list1.pop(-1)
-    print(num, end=' ')
+numbers = [1, 2, 3, 4, 5]
+# for i in range(0, len(list1)):
+#     num = list1.pop(-1)
+#     print(num, end=' ')
+
+def newlist(text):
+    output = []
+    for i in range(len(text)-1, -1, -1):
+        output.append(text[i])
+    return output
+print(newlist(numbers))
