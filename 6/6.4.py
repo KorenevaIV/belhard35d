2acb5c4e8cc4e81af5244a4b6a1dc98894ce12c1
KorenevaIV <bagira-87@mail.ru>
2022-11-14 16:14:34 +0300

    add solution

--- 6/6.4.py
+++ 6/6.4.py
@@ -2,10 +2,12 @@
 # образом, чтобы остались только строки, использование дополнительного списка
 # незаконно
 
-list = ['a','b','c', 5, 'g', 8, 'a']
+list = ['a','b','c', 5, 7, 'g', 8, 'a']
 
-for i in list:
-    if type(i) != str:
-        list.remove(i)
-print(list)
+# for i in list:
+#     if type(i) != str:
+#         list.remove(i)
+# print(list)
 
+new_list = ', '.join(filter(lambda s: str(s) in list, list))
+print(new_list)
