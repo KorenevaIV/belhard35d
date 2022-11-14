20a989fd45c59c4e7c4cf66dbcd285a3d0ef94f2
KorenevaIV <bagira-87@mail.ru>
2022-11-14 17:38:11 +0300

    add solution

--- 6/6.5.py
+++ 6/6.5.py
@@ -21,3 +21,14 @@ def newlist(num):
         newlist(num[:-1])
 
 newlist(numbers)
+
+
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+
+for i in frange(0.5, 1.0, 0.1):
+    print(i)
