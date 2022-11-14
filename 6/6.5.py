09db2808ed5e2800e291bfb190ef6572c9322d3a
KorenevaIV <bagira-87@mail.ru>
2022-11-14 17:38:59 +0300

    add solution

--- 6/6.5.py
+++ 6/6.5.py
@@ -4,31 +4,31 @@
 numbers = [1, 2, 3, 4, 5]
 
 
-# def newlist(text):
-#     output = []
-#     for i in range(len(text)-1, -1, -1):
-#         output.append(text[i])
-#     return output
+def newlist(text):
+    output = []
+    for i in range(len(text)-1, -1, -1):
+        output.append(text[i])
+    return output
+
+
+print(newlist(numbers))
+#
+# def newlist(num):
+#     if len(num) == 0:
+#         print('')
+#     else:
+#         print(num[-1], end=' ')
+#         newlist(num[:-1])
+#
+# newlist(numbers)
 #
 #
-# print(newlist(numbers))
-
-def newlist(num):
-    if len(num) == 0:
-        print('')
-    else:
-        print(num[-1], end=' ')
-        newlist(num[:-1])
-
-newlist(numbers)
-
-
-def frange(start, stop, step):
-    i = start
-    while i < stop:
-        yield i
-        i += step
-
-
-for i in frange(0.5, 1.0, 0.1):
-    print(i)
+# def frange(start, stop, step):
+#     i = start
+#     while i < stop:
+#         yield i
+#         i += step
+#
+#
+# for i in frange(0.5, 1.0, 0.1):
+#     print(i)
