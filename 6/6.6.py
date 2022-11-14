505a59acdef7e07de52e10c4fcfa16cd140cd016
KorenevaIV <bagira-87@mail.ru>
2022-11-14 20:28:11 +0300

    add solution

--- 6/6.6.py
+++ 6/6.6.py
@@ -4,7 +4,7 @@ randomlist = [1, 4, 6, 9, 15, 77, 32, 90, 101, 53, 86, 1, 4, 15]
 
 list = []
 for i in randomlist:
-    if i %2 == 0:
+    if i % 2 == 0:
         list.insert(0, i)
     else:
         list.append(i)
--- 6/6.8.py
+++ 6/6.8.py
@@ -1,2 +1,13 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
+dict = {'Belarus': ['Minsk', 'Grodno', 'Brest'],
+        'Poland': ['Gdansk', 'Belostok', 'Warsaw'],
+        'Ukrain': ['Lviv', 'Kiev', 'Rovno']
+        }
+
+txt = input('Enter name of the city: ')
+for key in dict.keys():
+        if txt in dict[key]:
+            print(key)
+
+
