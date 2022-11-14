cf027f84377ae71040308acbaf7ef9623b928deb
KorenevaIV <bagira-87@mail.ru>
2022-11-14 16:38:22 +0300

    add solution

--- 6/6.4.py
+++ 6/6.4.py
@@ -6,7 +6,6 @@
 numbers = ['a','b','c', 5, 7, 'g', 8, 'a']
 numbers = list(filter(lambda x: isinstance(x, str), numbers))
 # numbers = [i for i in numbers if isinstance(i, str)]
-# #
 #
 print(numbers)
 
