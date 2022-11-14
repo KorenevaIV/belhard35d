344ec513dad6db98c1f3b9b1cb903ba0b3e9d2c8
KorenevaIV <bagira-87@mail.ru>
2022-11-14 18:42:02 +0300

    add solution

--- 6/6.6.py
+++ 6/6.6.py
@@ -2,17 +2,13 @@
 # четные, потом нечётные
 randomlist = [1, 4, 6, 9, 15, 77, 32, 90, 101, 53, 86, 1, 4, 15]
 
-
-newlist = []
-newlist2 = []
-
+list = []
 for i in randomlist:
-    if i % 2 == 0:
-        newlist.append(i)
+    if i %2 == 0:
+        list.insert(0, i)
     else:
-        newlist2.append(i)
+        list.append(i)
 
-a = newlist + newlist2
-print(newlist)
-print(newlist2)
-print(a)
+
+print(list)
+
