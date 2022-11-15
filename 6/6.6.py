834078263f83ae2a17964f6f212e5efd2c9f9bd9
KorenevaIV <bagira-87@mail.ru>
2022-11-15 11:53:38 +0300

    add solution

--- 6/6.6.py
+++ 6/6.6.py
@@ -9,6 +9,26 @@ for i in randomlist:
     else:
         list.append(i)
 
-
 print(list)
 
+
+numbers = [2, 6, 3, 4, 3, 3, 5, 6, 8, 2, 34, 6, 4]
+
+
+def my_sort(numbers):
+    # result = []
+    # for i in range(len(numbers)):
+    #     if numbers[i] % 2:
+    #         result.append(numbers[i])
+    #     else:
+    #         result.insert(0, numbers[i])
+    # return result
+    # for i in range(len(numbers)):
+    #     if numbers[i] % 2 == 0:
+    #         numbers.insert(0, numbers.pop(i))
+    # return numbers
+    numbers = list(filter(lambda x: not x % 2, numbers)) + list(filter(lambda x: x % 2, numbers))
+    return numbers
+
+
+print(my_sort(numbers))
