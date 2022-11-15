0be513682d04e1ecaa680c6876104debae0459ab
KorenevaIV <bagira-87@mail.ru>
2022-11-15 13:22:17 +0300

    add solution

--- lessons/les 7.py
+++ lessons/les 7.py
@@ -31,19 +31,43 @@ vklad = int(input())
 procent = int(input())
 # ставка рефинансирования б через сколько увеличится в два раза
 
-def calculate_deposit(vklad: float | int, procent: float | int, k: float | int) -> int:
-    years = 0
-    sum_za_2 = vklad*k
-    procent /= 100
-    while vklad <= sum_za_2:
-        vklad += vklad * procent
-        vklad = round(vklad, 2)
-        years += 1
-        print(f'{years=} {vklad=}')
-    return years
-print(calculate_deposit(
-    float(input('deposit: ')),
-    float(input('percent: ')),
-    float(input('k: ')))
-)
+# def calculate_deposit(vklad: float | int, procent: float | int, k: float | int) -> int:
+#     years = 0
+#     sum_za_2 = vklad*k
+#     procent /= 100
+#     while vklad <= sum_za_2:
+#         vklad += vklad * procent
+#         vklad = round(vklad, 2)
+#         years += 1
+#         print(f'{years=} {vklad=}')
+#     return years
+# print(calculate_deposit(
+#     float(input('deposit: ')),
+#     float(input('percent: ')),
+#     float(input('k: ')))
+# )
+
+C2H5OH -> {'C':2, 'H': 6, 'O': 1}
+# C2H5OH -> {'C': 2, 'H': 6, 'O': 1}
+# H2O -> {'H': 2, 'O': 1}
+
+data = {}
+formula = 'C2H5OH'
+formula += '1' if formula[-1].isalpha() else ''
+print(formula)
+
+for i in range(len(formula)):
+    if formula[i].isalpha():
+        if formula[i+1].isdigit():
+            if formula[i] in data:
+                data[formula[i]] += int(formula[i+1])
+            else:
+                data[formula[i]] = int(formula[i+1])
+        else:
+            if formula[i] in data:
+                data[formula[i]] += 1
+            else:
+                data[formula[i]] = 1
+print(data)
+
 
