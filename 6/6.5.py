86e0c24a331eaf78de3296f44dff6f9c80881d5f
KorenevaIV <bagira-87@mail.ru>
2022-11-15 11:44:47 +0300

    add solution

--- 6/6.5.py
+++ 6/6.5.py
@@ -8,9 +8,8 @@ def my_reversed(lst):
     for i in range(len(lst) // 2):
         j = len(lst) - 1 - i  # индекс элемента с противоположной стороны
         lst[i], lst[j] = lst[j], lst[i]
-        # lst[i], lst[~i] = lst[~i], lst[i]
+        # lst[i], lst[~i] = lst[~i], lst[i] тильда дает противоположный индекс нашему i
     return lst
-
 
 print(my_reversed(numbers))
 #
