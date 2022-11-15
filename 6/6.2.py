9c54a546216add39f3f1b4fe09fca728bd9d38d4
KorenevaIV <bagira-87@mail.ru>
2022-11-15 11:15:43 +0300

    add solution

--- 6/6.2.py
+++ 6/6.2.py
@@ -10,34 +10,64 @@ morze = {
     's': '•••', 't': '—', 'u': '••—', 'v': '•••—', 'w': '•——', 'x': '—••—',
     'y': '—•——', 'z': '——••'
 }
-def Morse(s):
-    a = ''
-    for i in s.lower():
-        if i in morze.keys():
-            a += morze[i] + ' '
-        if i == ' ':
-            a += ' / '  # Слова разделяются слэшем
-    return a
+# def Morse(s):
+#     global morze
+#     a = ''
+#     for i in s.lower():
+#         if i in morze.keys():
+#             a += morze[i] + ' '
+#         if i == ' ':
+#             a += ' / '  # Слова разделяются слэшем
+#     return a
+#
+#
+# a = 'THello world how are you'
+# print(Morse(a))
+#
+#
+# #как назад вернуть в текст
+# def get_key(val, morze):  # Функция возвращает ключ словаря по значению
+#     for key, value in morze.items():
+#         if val == value:
+#             return key
+#
+# def MorseTxt(s):
+#     a, s = '', s.split()
+#     for i in s:
+#         if i in morze.values():
+#             a += get_key(i, morze)
+#         if i == '/':
+#             a += ' '
+#     return a
+#
+# print(MorseTxt(Morse(a)))
+
+def text_to_morse(text):
+    global morse
+    result = ''
+    text = text.lower()
+    for i in text:
+        if i in morse:
+            result += morse[i] + ' '
+        elif i == ' ':
+            result += '  '
+    return result
 
 
-a = 'THello world how are you'
-print(Morse(a))
+def morse_to_text(morse_text):
+    global morse
+    morse_text = morse_text.replace('   ', ' | ').split()
+    text = ''
+    for i in morse_text:
+        if i == '|':
+            text += ' '
+        else:
+            for key, val in morse.items():
+                if i == val:
+                    text += key
+                    break
+    return text
 
 
-#как назад вернуть в текст
-def get_key(val, morze):  # Функция возвращает ключ словаря по значению
-    for key, value in morze.items():
-        if val == value:
-            return key
-
-def MorseTxt(s):
-    a, s = '', s.split()
-    for i in s:
-        if i in morze.values():
-            a += get_key(i, morze)
-        if i == '/':
-            a += ' '
-    return a
-
-print(MorseTxt(Morse(a)))
+print(morse_to_text(text_to_morse(text='Hello world')))
 
--- 6/6.3.py
+++ 6/6.3.py
@@ -2,14 +2,21 @@
 # указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
 list  = [1,2,3,4,5,6,7]
 n = int(input())
-a = int(len(list))
+# a = int(len(list))
 
-if n <= a:
-    srez1 = list[0:(a-n)]
-    srez2 = list[-n:]
-    new_list = srez2 + srez1
-    print(new_list)
-else:
-    print('Число n  превышает длину списка.')
+# if n <= a:
+#     srez1 = list[0:(a-n)]
+#     srez2 = list[-n:]
+#     new_list = srez2 + srez1
+#     print(new_list)
+# else:
+#     print('Число n  превышает длину списка.')
 
 
+def list_offset(lst, n):
+    n -= len(lst) * (n // len(lst))
+    lst = lst[-n:] + lst[:-n]
+    return lst
+
+
+print(list_offset([1, 2, 3, 4, 5, 6, 7], -9))
