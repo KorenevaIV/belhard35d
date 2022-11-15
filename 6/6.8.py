8315a59e2e0e21cf1436b14e091eb656e81a0de9
KorenevaIV <bagira-87@mail.ru>
2022-11-15 12:02:09 +0300

    add solution

--- 6/6.8.py
+++ 6/6.8.py
@@ -11,3 +11,19 @@ for key in dict.keys():
             print(key)
 
 
+countries = {
+    'телемелитрямдия': ['нарния'],
+    'россия': ['москва', 'питер'],
+    'беларусь': ['минск', 'могилев', 'гомель']
+}
+
+
+def find_country(city):
+    global countries
+    for country, cities in countries.items():
+        if city.lower() in cities:
+            return country
+    return 'страна не найдена'
+
+
+print(find_country('могилев'))
