3c4046bc9d29399ca57a6bb0c8a7fd33bbc1541c
KorenevaIV <bagira-87@mail.ru>
2022-11-15 11:24:06 +0300

    add solution

--- 6/6.4.py
+++ 6/6.4.py
@@ -5,8 +5,20 @@
 
 numbers = ['a','b','c', 5, 7, 'g', 8, 'a']
 numbers = list(filter(lambda x: isinstance(x, str), numbers))
-# numbers = [i for i in numbers if isinstance(i, str)]
-#
+
+# numbers = list(filter(lambda x: isinstance(x, (str, int)), numbers))
+
 print(numbers)
 
-
+lst = [2, 3, 4, 5, 'fghj', True, [], None, 'dfgh', 'fgh']
+# var 1
+# lst = list(filter(lambda x: isinstance(x, str), lst))
+# print(lst)
+# var 2
+i = 0
+while i < len(lst):
+    if not isinstance(lst[i], str):
+        del lst[i]
+    else:
+        i += 1
+print(lst)
