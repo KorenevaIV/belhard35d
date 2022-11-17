82fa777846c96797bfb4acc92b2e2313f3c62823
KorenevaIV <bagira-87@mail.ru>
2022-11-17 17:38:47 +0300

    add solution

--- 7/7.3.py
+++ 7/7.3.py
@@ -1,2 +1,10 @@
 # 3. Написать функцию pow, которая принимает число А и число Б, необходимо с помощью
 # рекурсии возвести число А в степень Б
+def pow(number: int, power: int) -> int:
+    if (power == 1):
+        return (number)
+    if (power != 1):
+        return (number * pow(number, power - 1))
+number = int(input("Enter number: "))
+power = int(input("Enter power: "))
+print('Result: ', pow(number, power))
