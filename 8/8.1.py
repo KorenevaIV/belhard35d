90c946fe7d3cd8e49dc549210cfd1e44ab6fd4f5
KorenevaIV <bagira-87@mail.ru>
2022-11-21 20:34:07 +0300

    new data

--- 8/8.1.py
+++ 8/8.1.py
@@ -3,3 +3,12 @@
 # Написать метод __str__ возвращающая форматированную строку с данными об студенте
 # Написать функцию (не метод) student_sort принимающая список студентов: students:
 # list[Student] и возвращающая список студентов отсортированный по имени
+class Student:
+    def __init__(self, first_name: str, group: int,marks: list[int]):
+        self.first_name = first_name
+        self.group = group
+        self.marks = marks
+
+    def __str__(self):
+        data_about_student = f'{self.first_name} {self.group} {self.marks}'
+        return data_about_student
