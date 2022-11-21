0c87ca68eb101c8e0200f5a8e4a29eae768be91c
KorenevaIV <bagira-87@mail.ru>
2022-11-21 21:19:59 +0300

    new data

--- 8/8.1.py
+++ 8/8.1.py
@@ -4,7 +4,7 @@
 # Написать функцию (не метод) student_sort принимающая список студентов: students:
 # list[Student] и возвращающая список студентов отсортированный по имени
 class Student:
-    def __init__(self, first_name: str, group: int,marks: list[int]):
+    def __init__(self, first_name: str, group: int, marks: list[int]):
         self.first_name = first_name
         self.group = group
         self.marks = marks
@@ -12,3 +12,15 @@ class Student:
     def __str__(self):
         data_about_student = f'{self.first_name} {self.group} {self.marks}'
         return data_about_student
+
+user = Student('Alex', 45, [1, 2, 3])
+user2 = Student('Eva', 44, [4, 5, 6])
+user3 = Student('Ivan', 45, [5, 4, 3])
+
+
+
+def student_sort(self, list[Student]) -> list:
+    sorted(list, key = self.first_name)
+
+
+print(student_sort(list[Student]))
