d234047cc1414bfe704e9e239995ebac6fbf2128
KorenevaIV <bagira-87@mail.ru>
2022-11-27 18:41:58 +0300

    new data

--- 10/10.1.py
+++ 10/10.1.py
@@ -6,14 +6,16 @@ class Car:
     # is_baby_seat: bool (наличие десткого кресла)
     # is_busy: bool (определяется внутри конструктора со значением False, не принимается на
     # вход)
-    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool, is_busy = False) -> None:
+    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool) -> None:
         self.color = color
         self.count_passenger_seats = count_passenger_seats
         self.is_baby_seat = is_baby_seat
-        self.is_busy = is_busy
+        self.is_busy = False
+
 
     # 1.1 Написать магический метод __str__ выводящий форматированную строку с информацией
     # об автомобиле
+
     def __str__(self):
         return f'''Color: {self.color}. There are {self.count_passenger_seats} seats. 
         Seats for baby {"Yes" if self.is_baby_seat else "No"}, Busy: {"Yes" if self.is_busy else "No"}'''
@@ -22,9 +24,10 @@ class Car:
 # 2. Написать класс Taxi
 # Конструктор класса принимает атрибуты:
 # cars: list[Car] (список экземпляров класса Car)
-class Taxi:
-    def __init__(self, cars: list[Car]) -> None:
-        self.cars: list[Car] = []
+class Taxi(Car):
+    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool):
+        super().__init__(color, count_passenger_seats, is_baby_seat)
+        self.cars = list[Car]
 
     # 2.1 Реализовать метод find_car
     # На вход метода поступают атрибуты: count_passengers, is_baby (количество пассажиров,
@@ -33,11 +36,19 @@ class Taxi:
     # свободный (is_busy = False), у автомобиля сменить атрибут is_busy на значение True, если
     # подходящего автомобиля нет, метод должен возвращать None
     def find_car(self, count_passengers: int, is_baby: bool):
-
-                return self.is_busy = True
+        if count_passengers == self.count_passenger_seats and is_baby == self.is_baby_seat:
+            if self.is_busy is False:
+                self.is_busy = True
+                return self.cars
+            else:
+                return None
 
 
 
 car1 = Car('red', 4, True)
+car2 = Car('blue', 5, True)
+car3 = Car('black', 5, False)
+car4 = Car('white', 4, False)
+
 print(car1)
 
