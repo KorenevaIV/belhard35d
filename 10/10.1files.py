6b56c553fecc85a0c68c5791ad3d7dd03e816ca4
KorenevaIV <bagira-87@mail.ru>
2022-11-28 19:48:44 +0300

    new data

--- 10/10.1files.py
+++ 10/10.1files.py
@@ -14,11 +14,22 @@
 # print(lines[1])
 # file.close()
 #
-
-with open('text.txt', 'r', encoding= 'utf-8') as file:
-    lines = [line.strip() for line in file if line.strip()]
-
 n = int(input('Enter number: '))
+lines = []  # here will be lists of line[] in quantity 'n'
+with open('text.txt', 'r', encoding='utf-8') as file:
+    while True:
+        line = []  # here will number 'n' of lines from file
+        for _ in range(n):  # iterator will work 'n' times
+            text = file.readline()  # we will read first line
+            text = text.strip()  # then we delete /n from the end of line
+            if text:  # if text is True, it means there are words in this line
+                line.append(text)  # we add this text into line[], and we will repeat this action fot 'n' times
+            else:
+                n += 1
+                continue
+        lines.append(line)
+        if len(line) != n:
+            break
 
 
 print(lines)
