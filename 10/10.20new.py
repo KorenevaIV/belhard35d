50865ce31a9e56a75cb7c13a3629bee589723666
KorenevaIV <bagira-87@mail.ru>
2022-11-28 12:55:11 +0300

    new data

--- 10/10.20new.py
+++ 10/10.20new.py
@@ -26,7 +26,16 @@ class Category:
 # 3.3 Написать метод класса delete принимающий индекс категории в списке категорий и
 # удаляющий элемент из списка категорий на этом индексе, если нет элемента на таком
 # индексе, ничего не делать, метод ничего возвращать не должен
+    def delete(self, index: int):
+        for index in range(0, len(self.categories) - 1):
+            del self.categories[index]
+
 # 3.4 Написать метод update принимающий индекс категорий и новое название категории, если
 # нет элемента на таком индексе, то новая категория должна добавляться с учетом того, что
 # имена категорий уникальны, если новое имя категории нарушает уникальность в списке
 # категорий, вызвать исключение ValueError
+    def update(self, index: int, new_name: str):
+        if self.categories[index] not in self.categories:
+            if new_name not in self.categories:
+                return self.categories.append(new_name)
+        raise ValueError
