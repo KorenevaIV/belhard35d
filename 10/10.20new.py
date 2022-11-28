bce0bdb83bfed86a39871cc3c0268a55932c5f08
KorenevaIV <bagira-87@mail.ru>
2022-11-28 14:19:14 +0300

    new data

--- 10/10.20new.py
+++ 10/10.20new.py
@@ -2,13 +2,16 @@
 
 class Category:
 # Создать атрибут класса categories
+
     categories: list[str] = []
+    category_name: str
 
 # 3.1 Написать метод класса add принимающий на вход название категории, если такой
 # категории в атрибуте класса categories нет, добавить данную категорию в список и вернуть
 # индекс вхождения новой категории в списке. Если такая категория уже есть, вызвать ошибку
 # ValueError
     def add(self, category_name: str) -> int:
+
         if category_name in self.categories:
             raise ValueError
         else:
@@ -17,7 +20,7 @@ class Category:
 
 # 3.2 Написать метод класса get принимающий индекс и возвращающий категорию из списка
 # категорий на этом индексе, если нет элемента на таком индексе, вызвать атрибут ValueError
-    def get(self, index: int) -> str:
+    def get(self, index: int):
         if index in range(0, len(self.categories) - 1):
             return self.categories[index]
         else:
@@ -35,6 +38,7 @@ class Category:
 # нет элемента на таком индексе, то новая категория должна добавляться с учетом того, что
 # имена категорий уникальны, если новое имя категории нарушает уникальность в списке
 # категорий, вызвать исключение ValueError
+
     def update(self, index: int, new_name: str):
         if self.categories[index] not in self.categories:
             if new_name not in self.categories:
@@ -43,15 +47,18 @@ class Category:
 
 # 4. Изменить класс выше, список категорий должен содержать не просто имена категорий, а
 # словари с данными о каждой категории (name: str, is_published: bool)
+
+
 class NewCategory(Category):
+
     dict_of_category: dict = {}
     is_published = bool
 
 # 4.1 Добавить метод make_published принимающий индекс категории и меняющий значение
 # ключа is_published на True, если такого индекса нет, вызвать исключение ValueError
-    def make_published(self, index: int)
+    def make_published(self, index: int):
         if self.categories[index] in self.categories:
-            self.dict_of_category = dict({'name': self.categories[index], 'is_published': (self.is_published = True)})
+            self.dict_of_category = dict({'name': self.categories[index], 'is_published': self.is_published})
 
 
 # 4.2 Добавить метод make_unpublished принимающий индекс категории и меняющий
