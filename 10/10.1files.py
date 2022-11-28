82a9604ecf52ca82198cbf76c8d1ad47bf690643
KorenevaIV <bagira-87@mail.ru>
2022-11-28 15:27:34 +0300

    new data

--- 10/10.1files.py
+++ 10/10.1files.py
@@ -1,5 +1,17 @@
 # 1. Дан многострочный файл txt
 # а) разбить файл на N(вводится с клавиатуры) файлов построчно
 # б) разбить файл на несколько файлов по N строк
+n = int(input())
+file = open('text.txt', 'r', encoding= 'utf-8')
+lines = file.readlines()
+for n in range(0, n-1):
 
-with open('10/text.txt', 'w', encoding= 'utf-8') as file:
+
+
+print(lines[1])
+file.close()
+    # for n in range(0, n-1):
+    #     new_data = file.write(lines[n])
+    #     print(new_data)
+    #
+
