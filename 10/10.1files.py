9b845743521451a7306378ba13a631bb9f4103eb
KorenevaIV <bagira-87@mail.ru>
2022-11-28 18:24:48 +0300

    new data

--- 10/10.1files.py
+++ 10/10.1files.py
@@ -1,17 +1,24 @@
 # 1. Дан многострочный файл txt
 # а) разбить файл на N(вводится с клавиатуры) файлов построчно
 # б) разбить файл на несколько файлов по N строк
-n = int(input())
-file = open('text.txt', 'r', encoding= 'utf-8')
-lines = file.readlines()
-for n in range(0, n-1):
+# n = int(input())
+# file = open('text.txt', 'r', encoding= 'utf-8')
+# lines = [line.strip() for line in file]
+# for line in file:
+#     while n >0:
+#         new_f = open('file.txt', 'wt')
+#         new_f.write(lines[n])
+#         n -= 1
+#         new_f.close()
+#
+# print(lines[1])
+# file.close()
+#
+
+with open('text.txt', 'r', encoding= 'utf-8') as file:
+    lines = [line.strip() for line in file if line.strip()]
+
+n = int(input('Enter number: '))
 
 
-
-print(lines[1])
-file.close()
-    # for n in range(0, n-1):
-    #     new_data = file.write(lines[n])
-    #     print(new_data)
-    #
-
+print(lines)
