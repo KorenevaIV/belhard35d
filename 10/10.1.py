06f28154eb8bb4bc9d3144a65f75e17519dedf8a
KorenevaIV <bagira-87@mail.ru>
2022-11-29 10:56:43 +0300

    new data

--- 10/10.1.py
+++ 10/10.1.py
@@ -12,10 +12,8 @@ class Car:
         self.is_baby_seat = is_baby_seat
         self.is_busy = False
 
-
     # 1.1 Написать магический метод __str__ выводящий форматированную строку с информацией
     # об автомобиле
-
     def __str__(self) -> str:
         return f'''Color: {self.color}. There are {self.count_passenger_seats} seats. 
         Seats for baby {"Yes" if self.is_baby_seat else "No"}, Busy: {"Yes" if self.is_busy else "No"}'''
@@ -34,30 +32,23 @@ class Taxi:
     # На основании данных, вернуть объект Car из атрибута cars подходящий по параметрам и
     # свободный (is_busy = False), у автомобиля сменить атрибут is_busy на значение True, если
     # подходящего автомобиля нет, метод должен возвращать None
-
-
-def find_car(self, count_passengers: int, is_baby: bool) -> Car | None:
-    for car in self.cars:
-        if not car.is_busy and car.count_passenger_seats >= count_passengers:
-            if is_baby and car.is_baby_seat:
-                car.is_busy = True
-                return car
-            elif is_baby:
-                return None
-            else:
-                car.is_busy = True
-                return car
-
-
-
-
-
+    def find_car(self, count_passengers: int, is_baby: bool) -> Car | None:
+        for car in self.cars:
+            if not car.is_busy and car.count_passenger_seats >= count_passengers:
+                if is_baby and car.is_baby_seat:
+                    car.is_busy = True
+                    return car
+                elif not is_baby:
+                    car.is_busy = True
+                    return car
 
 
 car1 = Car('red', 4, True)
 car2 = Car('blue', 5, True)
 car3 = Car('black', 5, False)
 car4 = Car('white', 4, False)
+cars = [car4, car3, car2, car1]
+taxi = Taxi(cars)
+print(taxi.find_car(4, True))
 
-print(car1)
 
