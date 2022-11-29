5f23d4c49fee525f2174e9f56da60f45a09d1f06
KorenevaIV <bagira-87@mail.ru>
2022-11-29 10:33:56 +0300

    new data

--- 10/10.2files.py
+++ 10/10.2files.py
@@ -2,5 +2,16 @@
 # укажите, каких слов в нем больше: начинающихся на гласную или на
 # согласную букву (без учета регистра)?o
 
-with open('text.txt', 'r', encoding=uts-8)
+with open('text.txt', 'r', encoding= 'utf-8') as file:
+    text = file.readlines()
+
+glasn = 0
+sogl = 0
+
+for word in text:
+    if word[i][0] in 'а,и,е,ё,о,у,ы,э,ю,я':
+        glasn += 1
+    else:
+        sogl += 1
+
 
