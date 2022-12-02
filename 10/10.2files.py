418ad2ea00feccb88135a71bb304b4415cf9c223
KorenevaIV <bagira-87@mail.ru>
2022-12-02 11:57:44 +0300

    new data

--- 10/10.2files.py
+++ 10/10.2files.py
@@ -2,16 +2,24 @@
 # укажите, каких слов в нем больше: начинающихся на гласную или на
 # согласную букву (без учета регистра)?o
 
-with open('text.txt', 'r', encoding= 'utf-8') as file:
-    text = file.readlines()
 
-glasn = 0
-sogl = 0
-
-for word in text:
-    if word[i][0] in 'а,и,е,ё,о,у,ы,э,ю,я':
-        glasn += 1
+with open('numbers.txt', 'r', encoding='utf-8') as file:
+    print(file.read())
+    file.seek(0)
+    words = file.read().lower().replace('\n', '').split()
+vowels = 'ёуеэоаыяиюeyuioa'
+vowels_count = 0
+consonants_count = 0
+for word in words:
+    if word[0] in vowels:
+        vowels_count += 1
     else:
-        sogl += 1
+        consonants_count += 1
+if vowels_count > consonants_count:
+    print('vowels')
+elif consonants_count > vowels_count:
+    print('consonants')
+else:
+    print('equal')
 
 
