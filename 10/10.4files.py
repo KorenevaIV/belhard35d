d7f2703272a9e17a85fe89987481d5119b58ad0d
KorenevaIV <bagira-87@mail.ru>
2022-12-02 12:12:56 +0300

    new data

--- 10/10.4files.py
+++ 10/10.4files.py
@@ -3,3 +3,38 @@
 приведение типов значений атрибута price, провести валидацию данных
 с помощью Pydantic, не верные записи вывести в другой файл, а из
 списка удалить
+from pydantic import BaseModel
+
+
+class Product(BaseModel):
+    article: str
+    title: str
+    description: str = ''
+    price: float
+
+
+with open('products.csv', 'r', encoding='utf-8') as file:
+    headers = file.readline().strip().split(',')
+    products = []
+    invalid_product = []
+    for product in file:
+        values = product.strip().split(',')
+        product = dict(list(zip(headers, values)))
+        if not product['article']:
+            invalid_product.append(product)
+            continue
+        try:
+            product['price'] = float(product['price'])
+        except ValueError:
+            invalid_product.append(product)
+            continue
+        products.append(product)
+with open('invalid_product.csv', 'w', encoding='utf-8') as file:
+    headers = ','.join(headers)
+    for product in invalid_product:
+        values = ','.join(list(product.values()))
+        headers += f'\n{values}'
+    file.write(headers)
+
+products = list(map(lambda x: Product(**x), products))
+print(products)
