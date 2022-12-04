d7f1e42804e572ec8665b97e9d19af58c1377db8
KorenevaIV <bagira-87@mail.ru>
2022-12-04 12:57:41 +0300

    new data

--- 12/homeworksql.py
+++ 12/homeworksql.py
@@ -47,8 +47,8 @@ conn.commit()
 cur.execute('''
     CREATE TABLE IF NOT EXISTS order_items(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_id INTEGER NOT NULL
-    product_id INTEGER NOT NULL
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
     FOREIGN KEY(order_id) REFERENCES orders(id) ON DELETE CASCADE,
     FOREIGN KEY(product_id) REFERENCES products(id)
     );
