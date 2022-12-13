b1c1960b63f62eeeafcf660fe2ec50faa22b481a
koreneva <bagira-87@mail.ru>
2022-12-13 11:16:42 +0300

    new data

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
-import unittest
-
+# import unittest
+#
 from lessons.lesson15 import multiply
-class MainTestCase(unittest.TestCase):
+# class MainTestCase(unittest.TestCase):
+#
+#     def test_multiply(self):
+#         self.assertEqual(multiply(2,4), 8)
 
-    def test_multiply(self):
-        self.assertEqual(multiply(2,4), 8)
+def test_multiply():
+    assert  multiply(4,2) == 8
